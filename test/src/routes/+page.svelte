b31a7b6326c9ed815b142de47363f500f6268d0a
GHOST <ghostdevbusiness@gmail.com>
2024-12-30 22:19:45 +0000

    Revert "fix errors"
    This reverts commit 7d9a74d200c168571dc178dee2e0bbb2611ab93e.

---
+++ test/src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	let something: string = 1;
+</script>
+
+<div on:click={() => {}}></div>
+
+<input autofocus />
+
+<div accessKey="z" />
+
+<div
+	on:mouseenter={() => {
+		console.log('something');
+	}}>
+</div>
